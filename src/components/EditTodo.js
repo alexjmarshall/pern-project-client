764aa5fc2a0121c462a3864bb525aa1be7656c38
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2020-10-06 13:55:17 -0600

    Reset description only if click outside modal content

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 
 const EditTodo = ({todo}) => {
 //TODO fix event listener for clicking elsewhere to close modal and reset description -- see example from custom select window click
 
   const [description, setDescription] = useState(todo.description);
+  const modalContent = useRef();
 
   const updateDescription = async(e) => {
     e.preventDefault();
@@ -21,17 +22,23 @@ const EditTodo = ({todo}) => {
     }
   };
 
+  const onClickOutsideModal = e => {
+    if(modalContent.current.contains(e.target))
+      return;
+    setDescription(todo.description);
+  }
+
   return (
     <>
       {/* Trigger the modal with a button */}
       <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#modal-${todo.todo_id}`}>Edit</button>
 
       {/* Modal */}
-      <div id={`modal-${todo.todo_id}`} className="modal fade" role="dialog" onClick={() => setDescription(todo.description)}>
+      <div id={`modal-${todo.todo_id}`} className="modal fade" role="dialog" onClick={e => onClickOutsideModal(e)}>
         <div className="modal-dialog">
 
           {/* Modal content */}
-          <div className="modal-content">
+          <div className="modal-content" ref={modalContent}>
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
               <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
