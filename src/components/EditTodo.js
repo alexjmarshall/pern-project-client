0413d6d0c5fb97cb161750ff279eed3cd09dbc85
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2020-10-05 22:52:49 -0600

    Add updateDescription and event listeners to reset input when close modal

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -3,27 +3,43 @@ import React, {useState} from "react";
 const EditTodo = ({todo}) => {
   const [description, setDescription] = useState(todo.description);
 
+  const updateDescription = async(e) => {
+    e.preventDefault();
+    try {
+      const body = {description};
+      const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`,{
+        method: 'PUT',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      })
+      
+      window.location = '/';
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <>
       {/* Trigger the modal with a button */}
       <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#modal-${todo.todo_id}`}>Edit</button>
 
       {/* Modal */}
-      <div id={`modal-${todo.todo_id}`} className="modal fade" role="dialog">
+      <div id={`modal-${todo.todo_id}`} className="modal fade" role="dialog" onClick={() => setDescription(todo.description)}>
         <div className="modal-dialog">
 
           {/* Modal content */}
           <div className="modal-content">
             <div className="modal-header">
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
-              <h4 className="modal-title">Modal Header</h4>
+              <h4 className="modal-title">Edit Todo</h4>
+              <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
             </div>
             <div className="modal-body">
-              <input type="text" className="form-control" defaultValue={description} />
+              <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-warning" data-dismiss="modal">Edit</button>
-              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-warning" data-dismiss="modal" onClick={e => updateDescription(e)}>Edit</button>
+              <button type="button" className="btn btn-default" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
             </div>
           </div>
 
